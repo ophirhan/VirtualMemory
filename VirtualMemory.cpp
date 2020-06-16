091e313766723fd5b93a20d7c41b1757461c241d
ophirhan <ophir.han@mail.huji.ac.il>
2020-06-16 12:53:07 +0300

    Lets test!

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -2,11 +2,17 @@
 #include "PhysicalMemory.h"
 
 #define GET_LINE(VAR, I) (VAR * PAGE_SIZE) + I
+typedef struct Furthest{
+    uint64_t page_in;
+    uint64_t virt_furthest; // virt addr of victim
+    uint64_t orig_ln_of_furthest; // phys addr containing phys addr of furthest page
+    word_t phys_furthest;
+    word_t distance;
+} Furthest;
 
 
 uint64_t transVaddrToPaddr(uint64_t virtualAddress);
 
-word_t get_frame(uint64_t papa);
 
 void clearTable(uint64_t frameIndex) {
     for (uint64_t i = 0; i < PAGE_SIZE; ++i) {
@@ -25,6 +31,8 @@ int VMread(uint64_t virtualAddress, word_t *value) {
     return 1;
 }
 
+word_t get_free_frame(const uint64_t keep, const uint64_t virtualAddress);
+
 uint64_t transVaddrToPaddr(uint64_t virtualAddress) {
     word_t phys_addr = 0;
     int trim_left = (1LL << OFFSET_WIDTH) - 1;
@@ -34,7 +42,7 @@ uint64_t transVaddrToPaddr(uint64_t virtualAddress) {
         tb_line = tb_line & trim_left;
         PMread(GET_LINE(phys_addr, tb_line), &phys_addr);
         if (phys_addr == 0) {
-            phys_addr = get_frame(papa);
+            phys_addr = get_free_frame(papa, virtualAddress);
             clearTable(phys_addr);
             PMwrite(GET_LINE(papa, tb_line), phys_addr);
         }
@@ -44,40 +52,87 @@ uint64_t transVaddrToPaddr(uint64_t virtualAddress) {
     return frame_phys;
 }
 
-word_t get_frame_helper(uint64_t keep,
-                        uint64_t cur,
-                        int rec_depth,
-                        uint64_t virtualAddress,
-                        uint64_t *max_page) {
-    if(rec_depth >= TABLES_DEPTH){
-        return 0;
+void update_furthest(uint64_t cur_virt, Furthest &fr, word_t next_frame, uint64_t this_line) {
+    // calculate distance
+    uint64_t reg_distance = cur_virt - fr.page_in;
+    reg_distance = reg_distance > 0 ? reg_distance : -reg_distance;
+    uint64_t cyclic_distance = NUM_PAGES - reg_distance;
+    uint64_t cur_distance = cyclic_distance > reg_distance ? reg_distance : cyclic_distance;
+
+    // update accordingly
+    if(cur_distance > fr.distance){
+        fr.distance = cur_distance;
+        fr.virt_furthest = cur_virt;
+        fr.phys_furthest = next_frame;
+        fr.orig_ln_of_furthest = this_line;
     }
-    bool all_zero = true;
-    word_t cur_line = 0;
-    for (int i = 0; i < OFFSET_WIDTH; ++i) { // offset width might be larger todo
-        PMread(GET_LINE(cur, i), &cur_line);
-        if (cur_line != 0) {
-            all_zero = false;
-            uint64_t frame_addr = get_frame_helper(cur, cur_line,rec_depth + 1, virtualAddress, max_page);
-            if (frame_addr != 0) {
-                return frame_addr;
+}
+
+bool Inspect_leaf(const uint64_t cur_tb, uint64_t cur_virt, uint64_t &max_frame, Furthest &fr, word_t &next_frame) {
+    bool empty;
+    for (int i = 0; i < PAGE_SIZE; ++i) {// first table might be shorter, that's fine?
+        uint64_t this_line = GET_LINE(cur_tb, i);
+        PMread(this_line, &next_frame);
+        if (next_frame != 0) {
+            empty = false;
+            update_furthest(cur_virt, fr, next_frame, this_line);
+            if(next_frame > max_frame){
+                max_frame = next_frame;
             }
         }
     }
-    if(all_zero && keep != cur && cur != 0)
+    return empty;
+}
+
+word_t inspect_table(const uint64_t keep, const uint64_t origin_line, const uint64_t cur_tb,
+                     uint64_t cur_virt, const int rec_depth, uint64_t &max_frame, Furthest &fr) {
+    bool empty = true;
+    word_t next_frame = 0;
+    cur_virt = cur_virt << OFFSET_WIDTH;
+    if(rec_depth < TABLES_DEPTH){
+        for (int i = 0; i < PAGE_SIZE; ++i) {
+            uint64_t this_line = GET_LINE(cur_tb, i);
+            PMread(this_line, &next_frame);
+            if (next_frame != 0) {
+                empty = false;
+                // bad practice to refactor recursion call to another method.
+                uint64_t empty_tb = inspect_table(keep, this_line, next_frame, cur_virt + i,
+                                                  rec_depth + 1, max_frame, fr);
+                if (empty_tb != 0) {
+                    return empty_tb;
+                }
+                if(next_frame > max_frame){
+                    max_frame = next_frame;
+                }
+            }
+        }
+    }
+    else{
+        empty = Inspect_leaf(cur_tb, cur_virt, max_frame, fr, next_frame);
+    }
+    if(empty && keep != cur_tb)
     {
-        // upadte papa, return cur
+        PMwrite(origin_line, 0);
+        return cur_tb;
     }
     return 0;
 }
 
-word_t get_frame(uint64_t keep, uint64_t virtualAddress) {
-    uint64_t max_page = 0;
-    uint64_t free_frame = get_frame_helper(keep, 0, 0, virtualAddress, &max_page);
-    if (free_frame == 0) {
-        uint64_t victim = get_evict
-        PMevict()
+word_t get_free_frame(const uint64_t keep, const uint64_t virtualAddress) {
+    uint64_t max_frame = 0; // max frame in use
+    Furthest fr;
+    fr.distance = 0;
+    fr.page_in = virtualAddress;
+    uint64_t empty_tb = inspect_table(keep, 0, 0, 0, 1, max_frame, fr);
+    if (empty_tb != 0) {
+        return empty_tb;
     }
+    if(max_frame + 1 <= RAM_SIZE) { // todo <=?
+        return max_frame + 1;
+    }
+    PMwrite(fr.orig_ln_of_furthest, 0);
+    PMevict(fr.phys_furthest, fr.virt_furthest);
+    return fr.phys_furthest;
 }
 
 
